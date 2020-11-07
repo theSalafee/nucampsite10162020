50b45f47fc1fdb824f8330da1a8db69065b4f183
Naasir Salafee <naasirbush@icloud.com>
2020-11-06 17:14:44 -0800

    Week 5 Dependancies

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -77,7 +77,7 @@ class CommentForm extends Component {
 
   handleSubmit(values) {
     this.toggleModal();
-    console.log("Current State is: " + JSON.stringify(values));
+    //console.log("Current State is: " + JSON.stringify(values));
     alert("Current State is: " + JSON.stringify(values));
   }
 
