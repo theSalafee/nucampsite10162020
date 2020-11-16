af7ff2e0d4d5e90d99b41847547bb31e8cb257ea
Naasir Salafee <naasirbush@icloud.com>
2020-11-15 19:49:12 -0800

    setting up json-server for netlify

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,1 +1,3 @@
-export const baseUrl = "http://localhost:3001/";
+// export const baseUrl = "http://localhost:3001/";
+export const baseUrl =
+    "https://my-json-server.typicode.com/theSalafee/nucampsite10162020";
